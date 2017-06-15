a0c107f2ffe0f977cceb392059206ff7e257f73a
Yoan Haouzi <yoan.haouzi@evaneos.com>
2017-06-15 16:13:05 +0200

    commit presse car yoan a un pb

--- src/StringToInt.php
+++ src/StringToInt.php
@@ -20,5 +20,23 @@ class StringToInt implements StringToIntInterface
         if (empty($value)) {
             return 0;
         }
+
+        $values = array_map('intval',(explode(';', $value)));
+        if (count(array_unique($values)) === 5) {
+            return 0;
+        }
+
+        $arrayCountValues = array_count_values($values);
+        $arrayFiltered = array_filter($arrayCountValues, function ($value) {
+            return $value > 1;
+        });
+        $toKeep = max(array_keys($arrayFiltered));
+        $values = array_filter($values, function ($current) use ($toKeep) {
+            return $current == $toKeep;
+        });
+
+        return array_reduce($values, function ($before, $current) {
+            return $before + $current;
+        }, 0);
     }
 }
--- tests/unit/StringToIntTest.php
+++ tests/unit/StringToIntTest.php
@@ -40,9 +40,32 @@ class StringToIntTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Initialize the Unit Test data
+     * @test
      */
-    private function initializeData()
+    public function it_should_return_0_when_no_number_occurs_at_least_2_times()
     {
+        $dices = "1;2;3;4;5";
+        $value = $this->SUT->cast($dices);
+
+        $this->assertSame(0, $value);
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_return_the_sum_of_the_highest_number_when_occured_at_least_two_times()
+    {
+        $dices = '3;3;3;4;4';
+        $value = $this->SUT->cast($dices);
+
+        $this->assertEquals(8, $value);
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_()
+    {
+        
     }
 }
