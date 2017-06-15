0e13fd452c9e1d9bc3a9a4c7fff9ae654d0c3373
Antoine Lépée <alepee@gmail.com>
2017-06-15 18:01:29 +0200

    test: it_should_not_match_if_there_is_no_large_straight

---
+++ src/Combination/LargeStraightCombination.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kata\ScoreCalculator\Combination;
+
+use Kata\ScoreCalculator\DiceFace;
+use Kata\ScoreCalculator\RollOfDices;
+
+class LargeStraightCombination implements Combination
+{
+    public function match(RollOfDices $rollOfDices): bool
+    {
+        // TODO: Implement match() method.
+    }
+
+    public function getScore(RollOfDices $rollOfDices): int
+    {
+        // TODO: Implement getScore() method.
+    }
+}
